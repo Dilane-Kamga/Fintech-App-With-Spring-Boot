3e40e03120146d7a805b73a6aa1ed2bd909bad39
Dilane Kamga <dilanekamga777@hotmail.com>
2022-03-21 07:18:16 +0100

    LendingEngine microservice

---
+++ lendingengine/src/main/java/com/peerlender/lendingengine/LendingengineApplication.java
@@ -0,0 +1,25 @@
+package com.peerlender.lendingengine;
+
+import com.peerlender.lendingengine.domain.model.User;
+import com.peerlender.lendingengine.domain.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LendingengineApplication implements CommandLineRunner {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	public static void main(String[] args) {SpringApplication.run(LendingengineApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		userRepository.save(new User("Dilane","Dilane","Kamga", 19,"Software Engineer" ));
+		userRepository.save(new User("Cedric","Cedric","Kamga", 21,"Politic" ));
+		userRepository.save(new User("Nolan","Nolan","Fogue", 4,"Pupil" ));
+	}
+}
